6c95ee115b2403539c2761c2c1c9c0026dff73a9
TariqKichawele <tariqkichawele66@gmail.com>
2024-12-23 10:33:35 -0500

    Add authentication middleware and integrate Clerk for user management

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+   <ClerkProvider>
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        Header
+        <Header />
         {children}
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
@@ -22,5 +24,6 @@ export default function RootLayout({ children }) {
         </footer>
       </body>
     </html>
+   </ClerkProvider>
   );
 }
